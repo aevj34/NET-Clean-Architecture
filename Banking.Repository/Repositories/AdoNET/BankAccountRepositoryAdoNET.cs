a6e86bb3d48c7aec318544884ea1b0e036b87a08
Pedro J. Saba <pedrojsaba@gmail.com>
2017-11-04 12:50:59 -0500

    add code to set by parameter

--- Banking.Repository/Repositories/AdoNET/BankAccountRepositoryAdoNET.cs
+++ Banking.Repository/Repositories/AdoNET/BankAccountRepositoryAdoNET.cs
@@ -1,19 +1,48 @@
 ﻿using Banking.Domain.Repositories;
 using Banking.Domain.Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Data.SqlClient;
-using System.Threading.Tasks;
 using System.Data;
 
+// ReSharper disable once CheckNamespace
 namespace Banking.Repository.Repositories
 {
-    public class BankAccountRepositoryAdoNET:IBankAccountRepository
+    public class BankAccountRepositoryAdoNet:IBankAccountRepository
     {
+        public BankAccount FindByNumber2(string accountNumber)
+        {
+            var bankAccount = new BankAccount();            
+            const string cmdText = "SELECT [Bank_Account_Id],[number], [balance],[locked],[Customer_Id] FROM Bank_Account WHERE number = @account_number";
+            using (var con = new SqlConnection(Functions.GetConnectionString()))
+            {
+                var command = new SqlCommand(cmdText, con) { CommandType = CommandType.Text };
+                command.Parameters.AddRange(new[]
+                {
+                    new SqlParameter
+                    {
+                        SqlDbType = SqlDbType.VarChar,
+                        Direction = ParameterDirection.Input,
+                        ParameterName = "@account_number",
+                        Value = accountNumber
+                    }
+                });
+                con.Open();
 
-        public BankAccount findByNumber(string accountNumber)
+                using (var reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    while (reader.Read())
+                    {
+                        bankAccount.setId(reader.GetInt64(reader.GetOrdinal("Bank_Account_Id")));
+                        bankAccount.setNumber(reader.GetString(reader.GetOrdinal("number")));
+                        bankAccount.setBalance(reader.GetDecimal(reader.GetOrdinal("balance")));
+                        bankAccount.setIsLocked(reader.GetBoolean(reader.GetOrdinal("locked")));                       
+                    }
+                }
+            }
+            return bankAccount;            
+        }
+
+        public BankAccount FindByNumber(string accountNumber)
         {
             SqlConnection conn = new SqlConnection(Functions.GetConnectionString());
             String query = "SELECT [Bank_Account_Id],[number], [balance],[locked],[Customer_Id] FROM Bank_Account WHERE number = '" + accountNumber + "'";
@@ -41,7 +70,7 @@ namespace Banking.Repository.Repositories
 
         }
 
-        public BankAccount findByNumberLocked(string accountNumber)
+        public BankAccount FindByNumberLocked(string accountNumber)
         {
             throw new NotImplementedException();
         }
